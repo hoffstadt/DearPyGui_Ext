02eab59deaf9c97ed7437f3d380e96af089e31b6
Raphaël Doursenaud <rdoursenaud@gmail.com>
2022-05-02 03:32:47 +0200

    Update version
    Fixes #6

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from codecs import open
 import os
 
 
-wip_version = "0.9.3"
+wip_version = "0.9.4"
 
 def version_number():
     """This function reads the version number which is populated by github actions"""
